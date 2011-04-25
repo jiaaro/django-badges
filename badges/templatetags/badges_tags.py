8f5f8a797bc7b8c55f5c2cc436a95625c2f61281
jiaaro <devnull@localhost>
2011-04-25 14:20:55 -0400

    re-added template tags that got lost along the way

--- badges/templatetags/badges_tags.py
+++ badges/templatetags/badges_tags.py
@@ -24,3 +24,24 @@ def _badge_count(user_or_qs):
 @register.filter
 def number_awarded(badge, user_or_qs=None):
     return badge.number_awarded(user_or_qs)
+ 
+@register.filter
+def progress_start(badge):
+    return badge.meta_badge.progress_start
+ 
+@register.filter
+def progress_finish(badge):
+    return badge.meta_badge.progress_finish
+ 
+@register.filter
+def progress(badge, user):
+    return badge.meta_badge.get_progress(user)
+ 
+@register.filter
+def is_in_progress(badge, user):
+    return 0 < badge.meta_badge.get_progress(user) < progress_finish(badge) 
+ 
+@register.filter
+def progress_percentage(badge, user):
+    prog = badge.meta_badge.get_progress_percentage(user=user)
+    return max(min(prog, 100), 0)
