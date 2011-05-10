4777d3b58ea786b1c69982f7ac6580967d56d09b
jiaaro <devnull@localhost>
2011-05-09 21:03:03 -0400

    increment version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='django-badges',
-    version='0.1.5',
+    version='0.1.6',
     author='James Robert',
     author_email='jiaaro@gmail.com',
     description=('An easy to use app that provides Stack Overflow style badges'
