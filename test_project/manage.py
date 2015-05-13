b4e402205ff9497b41de2fec77549e33bcfe2695
igor <igor.kramaric@gmail.com>
2015-05-13 17:12:58 +0200

    test_project in path

--- test_project/manage.py
+++ test_project/manage.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+from os.path import dirname, abspath, join
+
+PROJECT_ROOT = abspath(dirname(__file__))
+sys.path.insert(0, join(PROJECT_ROOT, '..'))
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
 
     from django.core.management import execute_from_command_line
 
