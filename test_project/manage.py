ae928deb4d80d801c9bf5a57beb646b101975344
igor <igor.kramaric@gmail.com>
2015-05-13 17:43:53 +0200

    sys was missing

--- test_project/manage.py
+++ test_project/manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
+import sys
+import os
 
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
 if __name__ == "__main__":
-    import os
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
 
     from django.core.management import execute_from_command_line
